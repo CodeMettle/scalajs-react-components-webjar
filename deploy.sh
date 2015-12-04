d1063fc7653aeb6acc92f572a5c433e160e811fd
Steven Scott <steven@codemettle.com>
2015-12-04 12:53:23 -0500

    hardcode versions; add deploy script

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+REPOSITORY=internal
+VERSION=0.2.0
+
+npm install
+webpack
+mkdir -p target
+zip -r9 target/react-components.jar assets node_modules/css-loader styles
+mvn deploy:deploy-file -Durl=http://maven.codemettle.com/content/repositories/${REPOSITORY}/ -DrepositoryId=${REPOSITORY} -Dfile=target/react-components.jar -DgroupId=org.webjars -DartifactId=react-components -Dversion=${VERSION} -Dpackaging=jar
